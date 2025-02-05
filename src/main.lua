0a0ec530ee839a350128918d7f97d60cf9895b95
BakersDozenBagels <bakersdozenbagelsbergman@gmail.com>
2025-02-04 20:03:49 -0500

    Add Mod Icon

--- src/main.lua
+++ src/main.lua
@@ -3,3 +3,10 @@ assert(SMODS.load_file('src/joker.lua'))()
 assert(SMODS.load_file('src/back.lua'))()
 assert(SMODS.load_file('src/tag.lua'))()
 assert(SMODS.load_file('src/blind.lua'))()
+
+SMODS.Atlas {
+    key = "modicon",
+    path = "Icon.png",
+    px = 34,
+    py = 34
+}
