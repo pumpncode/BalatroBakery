282f1e21daa63d5ba89bb917404beb0c714c553c
BakersDozenBagels <bakersdozenbagelsbergman@gmail.com>
2025-03-07 11:08:28 -0500

    Update Bakery_play_hand_early to match its late counterpart

--- src/util.lua
+++ src/util.lua
@@ -157,7 +157,7 @@ Bakery_API.guard(function()
                 d_chips = ret.chips,
                 chips = chips,
                 dollars = ret.dollars,
-                card = ret.card or default_card,
+                card = ret.tag or default_card,
                 after = ret.after
             })
         end
