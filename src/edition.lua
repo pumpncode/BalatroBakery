a74423be0939e4e7ac53608ea2ab792f6b92abde
BakersDozenBagels <bakersdozenbagelsbergman@gmail.com>
2025-03-23 03:22:14 -0400

    Fix Carbon playing card phantoms

--- src/edition.lua
+++ src/edition.lua
@@ -8,17 +8,20 @@ SMODS.Edition {
     shader = 'Bakery_Carbon',
     weight = 0,
     calculate = function(self, card, context)
-        if not card.debuff and context.after and (not card.area or card.area ~= G.hand) and not card.ability.eternal then
-            G.E_MANAGER:add_event(Event({
-                func = (function()
-                    if SMODS.shatters(card) then
-                        card:shatter()
-                    else
-                        card:start_dissolve(nil, true)
-                    end
-                    return true
-                end)
-            }))
+        if not card.debuff and context.destroy_card and (not card.area or card.area ~= G.hand) and not card.ability.eternal then
+            return {
+                remove = true
+            }
+            -- G.E_MANAGER:add_event(Event({
+            --     func = (function()
+            --         if SMODS.shatters(card) then
+            --             card:shatter()
+            --         else
+            --             card:start_dissolve(nil, true)
+            --         end
+            --         return true
+            --     end)
+            -- }))
         end
     end
 }
